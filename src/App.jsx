cb8fbe2448bc5cd8189eb39b4b6a08a89e389dd3
yixiangyin <y1027020239@qq.com>
2025-07-11 19:48:21 +1000

    add initial todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-const App = ()  => {
-  
+import { useState } from "react";
+
+const createInitialTodos = () => {
+  const initialTodos = [];
+  for (let i = 0; i < 10; i++) {
+    initialTodos.push({ id: i, text: "Item " + (i + 1) });
+  }
+  return initialTodos;
+};
+
+const App = () => {
+  const [ todos, setTodos ] = useState(createInitialTodos);
   return (
     <>
-    <div>
-      <h1>Reminders</h1>
-    </div>
+      <div>
+        <h1>Reminders</h1>
+        <ul>
+          {todos.map((item) => (
+            <li key={item.id}>{item.text}</li>
+          ))}
+        </ul>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
