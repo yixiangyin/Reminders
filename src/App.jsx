32ea08519a12eb2b76b387965a60f42d3c0cd7aa
yixiangyin <y1027020239@qq.com>
2025-08-04 22:42:52 +1000

    add filteredtodos

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,62 @@
-import { useState } from "react";
+import { useState } from "react"
 
 const createInitialTodos = () => {
-  const initialTodos = [];
-  for (let i = 0; i < 10; i++) {
-    initialTodos.push({ id: i, text: "Item " + (i + 1) });
+  const initialTodos = []
+  for (let i = 0; i < 3; i++) {
+    initialTodos.push({ id: i, text: "Item " + (i + 1), isComplete: false })
   }
-  return initialTodos;
-};
+  return initialTodos
+}
 
 const App = () => {
-  const [todos, setTodos] = useState(createInitialTodos);
-  const [text, setText] = useState("");
+  const [todos, setTodos] = useState(createInitialTodos)
+  const [text, setText] = useState("")
+  const filteredTodos = todos.filter(todo => !todo.isComplete);
+  const toggle = (id) => {
+    const todo = todos.find((todo) => String(todo.id) === String(id));
+    const toggledTodo = {...todo, isComplete: !todo.isComplete}
+    setTodos(todos.map(t => t.id == toggledTodo.id ? toggledTodo: t))
+  }
+  const addTodo = () => {
+    setTodos([
+      {
+        id: todos.length,
+        text: text,
+      },
+      ...todos,
+    ])
+    setText("")
+  }
   return (
     <>
       <div>
         <h1>Reminders</h1>
         <input value={text} onChange={(e) => setText(e.target.value)} />
-        <button
-          onClick={() => {
-            setText("");
-            setTodos([
-              {
-                id: todos.length,
-                text: text,
-              },
-              ...todos,
-            ]);
-          }}
-        >
-          Add
-        </button>
+        <button onClick={addTodo}>Add</button>
         <ul>
-          {todos.map((item) => (
-            <li key={item.id}>{item.text}</li>
+          {filteredTodos.map((todo) => (
+            <li
+              key={todo.id}
+              // style={{
+              //   display: "flex",
+              //   alignItems: "center",
+              //   gap: "0.5em",
+              //   padding: "4px 0",
+              // }}
+            >
+              <input
+                type="checkbox"
+                value={todo.isComplete}
+                onChange={() => toggle(todo.id)}
+                // style={{ width: "16px", height: "16px", borderRadius: "50%" }}
+              />
+              {todo.text}
+            </li>
           ))}
         </ul>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
